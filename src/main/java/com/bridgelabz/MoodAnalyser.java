d4717e02671a2600a30e5e052936c7a4df066461
prajakta <prajaktamahajan789@gmail.com>
2021-10-29 00:08:10 +0530

    Added:Massage should Return sad

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+    public static String analyseMood(String message) {
+        if (message.toLowerCase().contains("sad")) {
+        }
+        return "Sad";
+    }
+}
